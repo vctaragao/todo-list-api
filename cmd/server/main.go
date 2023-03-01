b947e00cc586a1a90699a263f861b5e8c9e0fcdd
Victor Moraes de Aragão <vct.aragao@gmail.com>
2023-03-01 19:30:56 -0300

    fix: remove dinamic fetch db host

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,32 +1,20 @@
 package main
 
 import (
-	"os"
-
 	"github.com/labstack/echo/v4"
 	"github.com/vctaragao/todo-list-api/api/http"
 	"github.com/vctaragao/todo-list-api/internal"
 	"github.com/vctaragao/todo-list-api/storage"
 )
 
+const SQLITE_FILE_PATH = "todo_list.db"
+
 func main() {
-	dbHost := getDbHost()
-	repo := storage.NewSqLite(dbHost)
+	repo := storage.NewSqLite(SQLITE_FILE_PATH)
 
 	tl := internal.NewTodoList(repo)
 
 	e := http.RegisterRouter(echo.New(), tl)
 
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func getDbHost() string {
-	appEnv := os.Getenv("APP_ENV")
-	dbHost := os.Getenv("SQLITE_FILE_PATH")
-
-	if appEnv == "testing" {
-		dbHost = ":memory:"
-	}
-
-	return dbHost
 }
